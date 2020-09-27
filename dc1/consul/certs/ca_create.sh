3ca2c9e854c70cace55df8c8dc542aae1608be9e
Anthony Magner <anthony.magner@gmail.com>
2020-09-26 23:07:27 -0600

    asdf

---
+++ dc1/consul/certs/ca_create.sh
@@ -0,0 +1,1 @@
+consul tls ca create
--- dc1/consul/start_s2.sh
+++ dc1/consul/start_s2.sh
@@ -2,4 +2,4 @@
 #echo -e '\e[38;5;198m'"++++ Killing any old consul process"
 #sudo pkill -9 consul
 #echo -e '\e[38;5;198m'"++++ Starting Consul in Server Mode"
-sudo consul agent -config-file=../cfg/consul/consul_s2.json -config-dir=../cfg/consul.d
+sudo consul agent -boostrap-expect 3 -config-file=../cfg/consul/consul_s2.json -config-dir=../cfg/consul.d
