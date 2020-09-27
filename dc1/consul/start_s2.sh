dbe490eff0d56677eec1f8911160270d1fb850ab
Anthony Magner <anthony.magner@gmail.com>
2020-09-26 18:21:12 -0600

    asdf

---
+++ dc1/consul/start_s2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#echo -e '\e[38;5;198m'"++++ Killing any old consul process"
+#sudo pkill -9 consul
+#echo -e '\e[38;5;198m'"++++ Starting Consul in Server Mode"
+sudo consul agent -config-file=../cfg/consul/consul_s2.json -config-dir=../cfg/consul.d
