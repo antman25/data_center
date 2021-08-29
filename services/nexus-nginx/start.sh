e7560b19930c6f65e53630ea1a096242c103f897
Anthony Magner <anthony.magner@gmail.com>
2021-08-28 23:13:25 -0600

    asdf

--- services/nexus-nginx/start.sh
+++ services/nexus-nginx/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name nexus-nginx nexus-nginx:latest 
+docker run --privileged --network host --name nexus-nginx -p 443:443 nexus-nginx:latest 
---
+++ services/nexus-nginx/term.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -it nexus-nginx /bin/bash
