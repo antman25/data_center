9934004691c6119fafca2e1b44d206d2e986347d
Anthony Magner <anthony.magner@gmail.com>
2021-08-28 18:33:14 -0600

    asdf

--- services/jenkins/start.sh
+++ services/jenkins/start.sh
@@ -1,2 +1,1 @@
-docker pull jenkins/jenkins:lts
-docker run -p 8080:8080 -p 50000:50000 -v /mnt/scratch/test:/var/jenkins_home jenkins
+docker run --name jenkins -p 30000:8080 -p 50000:50000 -v /mnt/scratch/volumes/jenkins:/var/jenkins_home jenkins/jenkins:lts-jdk11
--- services/jenkins/start2.sh
+++
@@ -1,1 +0,0 @@
-docker run --name jenkins -p 30000:8080 -p 50000:50000 -v /mnt/scratch/volumes/jenkins:/var/jenkins_home jenkins/jenkins:lts-jdk11
