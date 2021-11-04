3d204a476010982de9baff5b37708e8e0f712a81
Anthony Magner <anthony.magner@gmail.com>
2021-11-04 12:15:22 -0600

    asdf

---
+++ terraform-library/ca/jenkins-app.tf
@@ -0,0 +1,34 @@
+
+resource "vault_pki_secret_backend_role" "jenkins-app-role" {
+  backend          = vault_mount.antlinux_local_v1_ica1_v1.path
+  name             = "jenkins-app-role"
+  ttl              = 3600
+  allow_ip_sans    = false
+  key_type         = "rsa"
+  key_bits         = 4096
+  allowed_domains  = ["jenkins.antlinux.local"]
+  allow_subdomains = false
+  allow_bare_domains = true
+}
+
+resource "vault_pki_secret_backend_cert" "jenkins-app-cert" {
+	depends_on = [vault_pki_secret_backend_role.jenkins-app-role]
+
+	backend = vault_mount.antlinux_local_v1_ica1_v1.path
+	name = vault_pki_secret_backend_role.jenkins-app-role.name
+
+	common_name = "jenkins.antlinux.local"
+}
+
+resource "local_file" "jenkins-app-key" {
+    content  = vault_pki_secret_backend_cert.jenkins-app-cert.private_key
+    filename = "certs/jenkins-app-key.pem"
+}
+
+resource "local_file" "jenkins-app-cert" {
+    content  = vault_pki_secret_backend_cert.jenkins-app-cert.certificate
+    filename = "certs/jenkins-app-cert.pem"
+}
+
+
+
---
+++ terraform-library/ca/nexus-app.tf
@@ -0,0 +1,34 @@
+
+resource "vault_pki_secret_backend_role" "nexus-app-role" {
+  backend          = vault_mount.antlinux_local_v1_ica1_v1.path
+  name             = "nexus-app-role"
+  ttl              = 3600
+  allow_ip_sans    = false
+  key_type         = "rsa"
+  key_bits         = 4096
+  allowed_domains  = ["nexus.antlinux.local"]
+  allow_subdomains = false
+  allow_bare_domains = true
+}
+
+resource "vault_pki_secret_backend_cert" "nexus-app-cert" {
+	depends_on = [vault_pki_secret_backend_role.nexus-app-role]
+
+	backend = vault_mount.antlinux_local_v1_ica1_v1.path
+	name = vault_pki_secret_backend_role.nexus-app-role.name
+
+	common_name = "nexus.antlinux.local"
+}
+
+resource "local_file" "nexus-app-key" {
+    content  = vault_pki_secret_backend_cert.nexus-app-cert.private_key
+    filename = "certs/nexus-app-key.pem"
+}
+
+resource "local_file" "nexus-app-cert" {
+    content  = vault_pki_secret_backend_cert.nexus-app-cert.certificate
+    filename = "certs/nexus-app-cert.pem"
+}
+
+
+
