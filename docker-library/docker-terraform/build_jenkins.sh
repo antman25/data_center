0b2edaa8f194951eb6f8922cf6302800914b45e4
Anthony Magner <anthony.magner@gmail.com>
2021-11-08 15:05:07 -0700

    asdf

---
+++ docker-library/docker-terraform/build_jenkins.sh
@@ -0,0 +1,7 @@
+env
+TIMESTAMP=`date +%Y%m%d-%H%M%S`
+REPO=nexus.antlinux.local:8443
+TAG=${REPO}/custom-base/terraform
+sudo docker build --tag ${TAG}:${TIMESTAMP} --tag ${TAG}:latest -f ${WORKSPACE}/docker-library/docker-terraform/Dockerfile ${WORKSPACE}/docker-terraform/docker-terraform && \
+sudo docker push ${TAG}:${TIMESTAMP} && \
+sudo docker push ${TAG}:latest
