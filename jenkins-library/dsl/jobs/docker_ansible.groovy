e50544340b8892c280493790b9472d62ffe03d9d
Anthony Magner <anthony.magner@gmail.com>
2021-11-07 19:15:11 -0700

    asdf

--- jenkins-library/dsl/jobs/docker_ansible.groovy
+++ jenkins-library/dsl/jobs/docker_ansible.groovy
@@ -1,16 +1,23 @@
 folder('build-root/docker-ansible')
 
 pipelineJob('docker-ansible-build') {
-    scm {
+
+  def repo = 'http://gitlab.antlinux.local/antman/data_center.git'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
         git {
-            remote {
-                name('gitlab')
-                url('http://gitlab.antlinux.local/antman/data_center.git')
-            }
-            extensions {
-                cleanAfterCheckout()
-                relativeTargetDirectory('data_center_download')
-            }
+          remote { url(repo) }
+          branches('master')
+          scriptPath('jenkins-library/docker-ansible/Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
         }
+
+      }
     }
+  }
 }
+
