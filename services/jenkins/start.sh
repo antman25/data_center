a8b9eb80f7b41745d92781b4059aeab830030f75
Anthony Magner <anthony.magner@gmail.com>
2021-11-08 14:34:38 -0700

    asdf

--- services/jenkins/start.sh
+++ services/jenkins/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f jenkins
-docker run -d --name jenkins -p 8080:8080 -p 50000:50000 -v /mnt/scratch/volumes/jenkins:/var/jenkins_home nexus.antlinux.local:8443/vendor-base/jenkins/jenkins:2.319
+docker run -d --privileged --name jenkins -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/scratch/volumes/jenkins:/var/jenkins_home nexus.antlinux.local:8443/vendor-base/jenkins/jenkins:2.319
