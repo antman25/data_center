e04e73ac83d548e579627a3fc7685801dc11e856
Anthony Magner <anthony.magner@gmail.com>
2021-11-08 14:55:09 -0700

    asdf

--- docker-library/docker-ansible/build_jenkins.sh
+++ docker-library/docker-ansible/build_jenkins.sh
@@ -2,6 +2,6 @@ env
 TIMESTAMP=`date +%Y%m%d-%H%M%S`
 REPO=nexus.antlinux.local:8443
 TAG=${REPO}/custom-base/ansible
-docker build --tag ${TAG}:${TIMESTAMP} --tag ${TAG}:latest . && \
+docker build --tag ${TAG}:${TIMESTAMP} --tag ${TAG}:latest -f {WORKSPACE}/docker-library/docker-ansible/Dockerfile {WORKSPACE}/docker-library/docker-ansible && \
 docker push ${TAG}:${TIMESTAMP} && \
 docker push ${TAG}:latest
