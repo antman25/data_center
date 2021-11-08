ff29d5809819cfb001b1be77e78e07c0c2c82e9c
Anthony Magner <anthony.magner@gmail.com>
2021-11-08 13:29:31 -0700

    asdf

--- jenkins-library/dsl/jobs/build_root.groovy
+++ jenkins-library/dsl/jobs/build_root.groovy
@@ -1,1 +1,22 @@
 folder('build-root')
+
+pipelineJob('build-root/build-master') {
+
+  def repo = 'http://gitlab.antlinux.local/antman/data_center.git'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master')
+          scriptPath('jenkins-library/build-master/Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+  }
+}
