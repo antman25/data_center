c5fe9ff80cb5fb8b43b674e85fd13e29921a8483
Anthony Magner <anthony.magner@gmail.com>
2021-11-07 19:09:35 -0700

    asfd

--- jenkins-library/dsl/jobs/docker_ansible.groovy
+++ jenkins-library/dsl/jobs/docker_ansible.groovy
@@ -1,1 +1,16 @@
 folder('build-root/docker-ansible')
+
+pipelineJob('docker-ansible-build') {
+    scm {
+        git {
+            remote {
+                name('gitlab')
+                url('http://gitlab.antlinux.local/antman/data_center.git')
+            }
+            extensions {
+                cleanAfterCheckout()
+                relativeTargetDirectory('data_center_download')
+            }
+        }
+    }
+}
