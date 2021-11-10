0663df6558b466d4632c6fd002d9d57395dc8c9d
Anthony Magner <anthony.magner@gmail.com>
2021-11-10 15:54:54 -0700

    asdf

--- jenkins-library/dsl/jobs/packer_base.groovy
+++ jenkins-library/dsl/jobs/packer_base.groovy
@@ -1,6 +1,30 @@
-folder('build-root/packer/base')
+folder('build-root/packer/baseos')
+folder('build-root/packer/baseos/centos8')
+folder('build-root/packer/baseos/centos8/CentOS-8.2.2004')
+folder('build-root/packer/baseos/centos8/CentOS-8.4.2105')
 
-pipelineJob('build-root/packer/base/build') {
+pipelineJob('build-root/packer/baseos/centos8/CentOS-8.2.2004/build') {
+
+  def repo = 'http://gitlab.antlinux.local:30080/antman/data_center.git'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master')
+          scriptPath('jenkins-library/packer/baseos/centos8/Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+  }
+}
+
+pipelineJob('build-root/packer/baseos/centos8/CentOS-8.2.2004/build') {
 
   def repo = 'http://gitlab.antlinux.local:30080/antman/data_center.git'
 
