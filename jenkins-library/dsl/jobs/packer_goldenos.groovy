f907d2d902f2cf3cac5769779962027cb9ab70f6
Anthony Magner <anthony.magner@gmail.com>
2021-11-11 18:10:32 -0700

    asdf

--- jenkins-library/dsl/jobs/packer_goldenos.groovy
+++ jenkins-library/dsl/jobs/packer_goldenos.groovy
@@ -1,9 +1,59 @@
-folder('build-root/packer/golden-os')
-folder('build-root/packer/golden-os/centos8')
-folder('build-root/packer/golden-os/centos8/CentOS-8.2.2004')
-folder('build-root/packer/golden-os/centos8/CentOS-8.4.2105')
+folder('build-root/packer/golden-baseos')
+folder('build-root/packer/golden-baseos/winsvr2019')
+folder('build-root/packer/golden-baseos/winsvr2019/winsvr2019-JAN2021')
 
-pipelineJob('build-root/packer/golden-os/centos8/CentOS-8.2.2004/build') {
+folder('build-root/packer/golden-baseos/centos7')
+folder('build-root/packer/golden-baseos/centos7/CentOS-7.9.2009')
+
+folder('build-root/packer/golden-baseos/centos8')
+folder('build-root/packer/golden-baseos/centos8/CentOS-8.2.2004')
+folder('build-root/packer/golden-baseos/centos8/CentOS-8.4.2105')
+
+pipelineJob('build-root/packer/golden-baseos/winsvr2019/winsvr2019-JAN2021/build') {
+
+  def repo = 'http://gitlab.antlinux.local:30080/antman/data_center.git'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master')
+          scriptPath('jenkins-library/packer/base/Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+  }
+}
+
+
+pipelineJob('build-root/packer/golden-baseos/centos7/CentOS-7.9.2009/build') {
+
+  def repo = 'http://gitlab.antlinux.local:30080/antman/data_center.git'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master')
+          scriptPath('jenkins-library/packer/base/Jenkinsfile')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+      }
+    }
+  }
+}
+
+
+pipelineJob('build-root/packer/golden-baseos/centos8/CentOS-8.2.2004/build') {
 
   def repo = 'http://gitlab.antlinux.local:30080/antman/data_center.git'
 
@@ -29,7 +79,7 @@ pipelineJob('build-root/packer/golden-os/centos8/CentOS-8.2.2004/build') {
   }
 }
 
-pipelineJob('build-root/packer/golden-os/centos8/CentOS-8.4.2105/build') {
+pipelineJob('build-root/packer/golden-baseos/centos8/CentOS-8.4.2105/build') {
 
   def repo = 'http://gitlab.antlinux.local:30080/antman/data_center.git'
 
