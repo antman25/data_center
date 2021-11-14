a5e75b901749a39d9788ad81ee21284b7120274d
Anthony Magner <anthony.magner@gmail.com>
2021-11-13 20:27:01 -0700

    Development

--- services/gitlab/start.sh
+++ services/gitlab/start.sh
@@ -1,10 +1,12 @@
 export GITLAB_HOME=/mnt/scratch/volumes/gitlab
 sudo docker rm -f gitlab
 sudo docker run --detach \
-  --hostname 172.17.0.2 \
-  --publish 30443:443 --publish 30080:80 --publish 30022:22 \
+  --hostname gitlab.antlinux.local \
+  --publish 30443:443 --publish 30080:30080 --publish 30022:22 \
+  --privileged \
   --name gitlab \
   --restart always \
+  -e EXTERNAL_URL="http://gitlab.antlinux.local:30080" \
   --volume $GITLAB_HOME/config:/etc/gitlab \
   --volume $GITLAB_HOME/logs:/var/log/gitlab \
   --volume $GITLAB_HOME/data:/var/opt/gitlab \
