93bb2ffac64d79e361f70fe249cec21b71e85627
Anthony Magner <anthony.magner@gmail.com>
2021-11-21 15:03:12 -0700

    Development

--- packer-library/centos/scripts/build_stage01.sh
+++ packer-library/centos/scripts/build_stage01.sh
@@ -8,5 +8,5 @@ docker run \
       -v ${PWD}/../stage01:/packer \
       nexus.antlinux.local:8443/custom-base/hashicorp/packer:latest \
       build -color=false -timestamp-ui -on-error=cleanup -force \
-      -var-file=vars/centos7-7.9.2009.hcl \
+      -var-file=$1 \
       -only="stage01.vsphere-iso.centos_iso" .
