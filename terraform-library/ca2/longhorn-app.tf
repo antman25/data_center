48f34d50e38b9496b9f0b8ab294788f8be762f9f
Anthony Magner <anthony.magner@gmail.com>
2023-04-08 21:56:56 -0600

    asdf

--- terraform-library/ca2/longhorn-app.tf
+++ terraform-library/ca2/longhorn-app.tf
@@ -6,7 +6,7 @@ resource "vault_pki_secret_backend_role" "longhorn-app-role" {
   allow_ip_sans    = false
   key_type         = "rsa"
   key_bits         = 2048
-  allowed_domains  = ["longhorn.antlinux.org"]
+  allowed_domains  = ["longhorn.antlinux.org", "longhorn.antlinux.local"]
   allow_subdomains = false
   allow_bare_domains = true
 }
